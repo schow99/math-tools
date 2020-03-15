15bf5ed7eedabe3423fbcb3ab227dc6b7147d724
schow99 <samuelchow93@gmail.com>
2020-03-15 19:58:48 -0400

    Updates

---
+++ precache-manifest.5dd02b13f673e7bd8748ad2627270c6d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76ca85c48a5c46a3ad3c1a10ca0d049c",
+    "url": "/math-tools/index.html"
+  },
+  {
+    "revision": "972ee3cb2ef1933a0510",
+    "url": "/math-tools/static/css/main.20b4d664.chunk.css"
+  },
+  {
+    "revision": "4ef1bf6939537bc04f7e",
+    "url": "/math-tools/static/js/2.2985ed25.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/math-tools/static/js/2.2985ed25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "972ee3cb2ef1933a0510",
+    "url": "/math-tools/static/js/main.24d45876.chunk.js"
+  },
+  {
+    "revision": "f5e23333c59d1b0674e0",
+    "url": "/math-tools/static/js/runtime-main.baf11536.js"
+  }
+]);
