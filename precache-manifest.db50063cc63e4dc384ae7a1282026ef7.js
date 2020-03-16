87aa44496f642123087bdcdf89eb19298e0d8488
schow99 <samuelchow93@gmail.com>
2020-03-15 20:08:37 -0400

    Updates

--- precache-manifest.db50063cc63e4dc384ae7a1282026ef7.js
+++ precache-manifest.db50063cc63e4dc384ae7a1282026ef7.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "76ca85c48a5c46a3ad3c1a10ca0d049c",
+    "revision": "f77328dbbeca7af25a43f9177f0d55c0",
     "url": "/math-tools/index.html"
   },
   {
-    "revision": "972ee3cb2ef1933a0510",
+    "revision": "61ae27cc3549378b07a4",
     "url": "/math-tools/static/css/main.20b4d664.chunk.css"
   },
   {
-    "revision": "4ef1bf6939537bc04f7e",
-    "url": "/math-tools/static/js/2.2985ed25.chunk.js"
+    "revision": "7c61c74794294833395f",
+    "url": "/math-tools/static/js/2.86a8ce48.chunk.js"
   },
   {
     "revision": "f231859d6585c4cd5f80c344783ed269",
-    "url": "/math-tools/static/js/2.2985ed25.chunk.js.LICENSE.txt"
+    "url": "/math-tools/static/js/2.86a8ce48.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "972ee3cb2ef1933a0510",
-    "url": "/math-tools/static/js/main.24d45876.chunk.js"
+    "revision": "61ae27cc3549378b07a4",
+    "url": "/math-tools/static/js/main.b9cbdfee.chunk.js"
   },
   {
     "revision": "f5e23333c59d1b0674e0",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/math-tools/precache-manifest.5dd02b13f673e7bd8748ad2627270c6d.js"
+  "/math-tools/precache-manifest.db50063cc63e4dc384ae7a1282026ef7.js"
 );
 
 self.addEventListener('message', (event) => {
